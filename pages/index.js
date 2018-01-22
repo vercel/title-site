4d29131be32245bb22e091094cd0d21c34a04ded
Leo Lamprecht <mindrun@icloud.com>
2018-01-22 12:05:12 +0100

    Sweet styling

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 // Packages
 import { PureComponent } from 'react'
 import convertTitle from '@zeit/title'
+import AutosizeInput from 'react-input-autosize'
 
 class Index extends PureComponent {
   constructor(props) {
@@ -32,12 +33,96 @@ class Index extends PureComponent {
       onChange: this.handleChange,
       placeholder: 'Paste or Enter Your Title',
       autoComplete: 'off',
+      autoCorrect: 'off',
       spellCheck: false
     }
 
     return (
       <main>
-        <input {...settings} />
+        <section>
+          <h1>Capitalize Your Title</h1>
+
+          <p>Enter your title below to get it capitalized properly
+          according to
+          the <a href="http://www.chicagomanualofstyle.org" target="_blank">The Chicago Manual of Style</a>:</p>
+
+          <AutosizeInput {...settings} />
+        </section>
+
+        <style jsx global>{`
+          body {
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+            text-rendering: optimizeLegibility;
+            -webkit-font-smoothing: antialiased;
+          }
+
+          input {
+            min-width: 300px;
+            max-width: 620px;
+            box-sizing: border-box;
+            padding: 9.5px 15px;
+            border: 0;
+            text-align: center;
+            border-bottom: 1px solid #d8d8d8;
+            font-size: 14px;
+            transition: border-bottom-color 100ms ease-in, color 100ms ease-in;
+          }
+
+          input:focus {
+            outline: none;
+            border-color: #000;
+          }
+        `}</style>
+
+        <style jsx>{`
+          main {
+            position: absolute;
+            left: 0;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          section {
+            text-align: center;
+          }
+
+          h1 {
+            font-weight: normal;
+            font-size: 24px;
+            text-align: center;
+            margin-bottom: 25px;
+          }
+
+          aside {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding-top: 50px;
+          }
+
+          p {
+            font-weight: 400;
+            font-size: 14px;
+            line-height: 24px;
+            max-width: 390px;
+            text-align: center;
+            margin: 14px auto 30px auto;
+          }
+
+          a {
+            text-decoration: none;
+            color: #ff0080;
+          }
+
+          a:hover {
+            text-decoration: underline;
+          }
+        `}</style>
       </main>
     )
   }
