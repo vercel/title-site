a7bad8e79b56c0df5f73fda9f874b295d83ff11d
Juri Adams <juri@adams.sh>
2023-06-16 14:00:03 +0200

    Added missing `maximum-scale` attribute

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,10 @@ const Index = () => {
       <Head>
         <title>Capitalize Your Title</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1"
+        />
       </Head>
 
       <section>
